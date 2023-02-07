96a69a0d086b7b8c1d2975db1b962a70a62d309b
Amir Sohail <amirsohail335@gmail.com>
2023-02-07 12:04:53 +0530

    committed

--- function.js
+++ function.js
@@ -1,16 +1,15 @@
-function fun(x){
-    console.log(x+10);
-    return x+10;
+function fun(x) {
+  console.log(x + 10);
+  return x + 10;
 }
 
 let x = fun(10);
-console.log(fun(10));
-fun(10); 
+// console.log(fun(10));
+fun(10);
 
-function toCelsius(fahrenheit){
-    // return (5/9) * (fahrenheit-32);
-    console.log((5/9) * (fahrenheit-32));
-
+function toCelsius(fahrenheit) {
+  // return (5/9) * (fahrenheit-32);
+  console.log((5 / 9) * (fahrenheit - 32));
 }
 
-toCelsius(198)
+toCelsius(198);
